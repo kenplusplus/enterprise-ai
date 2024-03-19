9b4c3662055560b9b47de73f2a8818c1c147fca8
Lu Ken <ken.lu@intel.com>
2024-03-19 22:53:57 +0800

    initialize
    Signed-off-by: Lu Ken <ken.lu@intel.com>

---
+++ README.md
@@ -0,0 +1,15 @@
+# Home AI
+
+## Setup TTS Server
+
+Use [OpenTTS](https://github.com/synesthesiam/opentts) setup the TTS server
+
+```shell
+docker run -it -p 5500:5500 synesthesiam/opentts:all
+```
+
+Access:
+
+Web: http://localhost:5500/
+
+API: https://github.com/synesthesiam/opentts?tab=readme-ov-file#http-api-endpoints
---
+++ containers/whisper-server/whisper_client.py
@@ -0,0 +1,42 @@
+import os
+import requests
+import logging
+import argparse
+
+"""
+curl -F "file=@/path/to/file" http://localhost:5000/whisper
+"""
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+                    prog='whisper_client',
+                    description='Client to test whisper server')
+    parser.add_argument('-f', '--file', dest="audio_file", required=True)
+    parser.add_argument('-u', '--url', dest="server_url", default="http://localhost:5000/whisper")
+    args = parser.parse_args()
+    return args
+
+def send_requests(filepath, server_url):
+    files = {
+        'file': open(filepath, 'rb'),
+    }
+    response = requests.post(server_url, files=files)
+    print("HTTP Response", response.json())
+    results = response.json()['results'][0]
+    return results['transcript']
+
+if __name__=="__main__":
+    logging.basicConfig()
+    logging.getLogger().setLevel(logging.DEBUG)
+
+    args = parse_args()
+    filepath = os.path.realpath(args.audio_file)
+    assert filepath, "audio file %s not exists" % filepath
+
+    print("======================================")
+    print(" Whisper Test Client: ")
+    print(" Audio file:      ", filepath)
+    print(" Server URL:      ", args.server_url)
+    print("======================================")
+    data = send_requests(filepath, args.server_url)
+    print(data)
