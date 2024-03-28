98cc8b07753cc8df5796f22cce937c9c5f4704e8
Lu Ken <ken.lu@intel.com>
2024-03-28 21:55:11 +0800

    load all all language model at startup time
    Signed-off-by: Lu Ken <ken.lu@intel.com>

--- containers/ken-whisperx-server/app.py
+++ containers/ken-whisperx-server/app.py
@@ -4,7 +4,6 @@ from flask import Flask, abort, request
 from tempfile import NamedTemporaryFile
 
 #device = "cuda"
-audio_file = "audio.mp3"
 batch_size = 16 # reduce if low on GPU mem
 compute_type = "int8" # change to "int8" if low on GPU mem (may reduce accuracy)
 
@@ -16,6 +15,19 @@ DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 model = whisperx.load_model("small", device=DEVICE, compute_type=compute_type)
 # 3. Assign speaker labels
 diarize_model = whisperx.DiarizationPipeline(device=DEVICE)
+align_model_en_model, align_model_en_metadata = whisperx.load_align_model('en', device=DEVICE)
+align_model_zh_model, align_model_zh_metadata = whisperx.load_align_model('zh', device=DEVICE)
+
+align_models = {
+    'en': {
+        'model': align_model_en_model,
+        'meta': align_model_en_metadata
+    },
+    'zh': {
+        'model': align_model_zh_model,
+        'meta': align_model_zh_metadata
+    }
+}
 
 app = Flask(__name__)
 
@@ -43,13 +55,19 @@ def handler():
 
 
         # 2. Align whisper output
-        model_a, metadata = whisperx.load_align_model(language_code=result["language"], device=DEVICE)
-        result = whisperx.align(result["segments"], model_a, metadata, audio, DEVICE, return_char_alignments=False)
+        language = result["language"]
+        if language not in align_models:
+            print("not find the language %s" % language)
+            abort(400)
+
+        result = whisperx.align(result["segments"],
+                                align_models[language]['model'],
+                                align_models[language]['meta'],
+                                audio,
+                                DEVICE,
+                                return_char_alignments=False)
 
         print(result["segments"]) # after alignment
-
-        # delete model if low on GPU resources
-        # import gc; gc.collect(); torch.cuda.empty_cache(); del model_a
 
         # add min/max number of speakers if known
         diarize_segments = diarize_model(audio)
